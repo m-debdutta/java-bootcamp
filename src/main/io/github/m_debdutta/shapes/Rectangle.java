aea5bb674d21bb71812c43b0844d087245dd16e6
Debdutta <m-debdutta@users.noreply.github.com>
2023-11-10 15:06:01 +0530

    Sauma/Debu: Refactor createSqure for rectangle.

--- src/main/io/github/m_debdutta/shapes/Rectangle.java
+++ src/main/io/github/m_debdutta/shapes/Rectangle.java
@@ -12,14 +12,14 @@ public class Rectangle implements TwoDimensionalShape {
     this.breadth = breadth;
   }
 
-  static Rectangle createSquare(double length, double breadth) throws InvalidDimensionException {
+  public static Rectangle create(double length, double breadth) throws InvalidDimensionException {
     if (length < 0 || breadth < 0) throw new InvalidDimensionException();
     return new Rectangle(length, breadth);
   }
 
   public static Rectangle createSquare(double side) throws InvalidDimensionException {
     if (side < 0) throw new InvalidDimensionException();
-    return Rectangle.createSquare(side, side);
+    return Rectangle.create(side, side);
   }
 
   @Override
--- src/test/io/github/m_debdutta/shapes/RectangleTest.java
+++ src/test/io/github/m_debdutta/shapes/RectangleTest.java
@@ -9,31 +9,31 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class RectangleTest {
   @Test
   void areaShouldBeOneForUnitDimensions() throws InvalidDimensionException {
-    Rectangle rectangle = Rectangle.createSquare(1, 1);
+    Rectangle rectangle = Rectangle.create(1, 1);
     assertEquals(1, rectangle.area());
   }
 
   @Test
   void areaShouldBeProductOfLengthAndBreadth() throws InvalidDimensionException {
-    Rectangle rectangle = Rectangle.createSquare(5, 4);
+    Rectangle rectangle = Rectangle.create(5, 4);
     assertEquals(20, rectangle.area());
   }
 
   @Test
   void perimeterShouldBeFourForUnitDimensions() throws InvalidDimensionException {
-    Rectangle rectangle = Rectangle.createSquare(1, 1);
+    Rectangle rectangle = Rectangle.create(1, 1);
     assertEquals(4, rectangle.perimeter());
   }
 
   @Test
   void perimeterShouldBeDoubleOfSumOfLengthAndBreadth() throws InvalidDimensionException {
-    Rectangle rectangle = Rectangle.createSquare(5, 4);
+    Rectangle rectangle = Rectangle.create(5, 4);
     assertEquals(18, rectangle.perimeter());
   }
 
   @Test
   void shouldNotCreateRectangleWithNegativeDimensions() {
-    InvalidDimensionException exception = assertThrows(InvalidDimensionException.class, () -> Rectangle.createSquare(-4, 2));
+    InvalidDimensionException exception = assertThrows(InvalidDimensionException.class, () -> Rectangle.create(-4, 2));
   }
 
   @Test
