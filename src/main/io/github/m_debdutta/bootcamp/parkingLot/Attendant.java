2784e65c94da5146328332db1666888078d51109
Debdutta <m-debdutta@users.noreply.github.com>
2023-11-13 15:51:28 +0530

    Swagato/Debu: Add feature for an attandant to handle multiple parking lot

--- src/main/io/github/m_debdutta/bootcamp/parkingLot/Attendant.java
+++ src/main/io/github/m_debdutta/bootcamp/parkingLot/Attendant.java
@@ -1,4 +1,32 @@
 package io.github.m_debdutta.bootcamp.parkingLot;
 
+import io.github.m_debdutta.bootcamp.parkingLot.exception.ParkingLotFullException;
+
+import java.util.ArrayList;
+
 public class Attendant {
+  private final ArrayList<ParkingLot> parkingLots;
+
+  public Attendant(ArrayList<ParkingLot> parkingLots) {
+    this.parkingLots = parkingLots;
+  }
+
+  private ParkingLot getFreeParkingLot() throws ParkingLotFullException {
+    int index = 0;
+    while (index < this.parkingLots.size()) {
+      ParkingLot currentParkingLot = this.parkingLots.get(index);
+      if (!currentParkingLot.isFull()) {
+        return currentParkingLot;
+      }
+      index++;
+    }
+    throw new ParkingLotFullException();
+  }
+
+  //TODO: should parkinglot return boolean??
+
+  public boolean park() throws ParkingLotFullException {
+    ParkingLot freeParkingLot = this.getFreeParkingLot();
+    return freeParkingLot.park();
+  }
 }
--- src/test/io/github/m_debdutta/bootcamp/parkingLot/AttendantTest.java
+++ src/test/io/github/m_debdutta/bootcamp/parkingLot/AttendantTest.java
@@ -1,5 +1,29 @@
 package io.github.m_debdutta.bootcamp.parkingLot;
 
+import io.github.m_debdutta.bootcamp.parkingLot.exception.ParkingLotFullException;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class AttendantTest {
 
+  @Test
+  void shouldBeAbleToParkWhenThereAreAvailableSpaces() throws ParkingLotFullException {
+    ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+    parkingLots.add(new ParkingLot(1));
+    Attendant attendant = new Attendant(parkingLots);
+    assertTrue(attendant.park());
+  }
+
+  @Test
+  void shouldHandleMoreThanOneParkingLot() throws ParkingLotFullException {
+    ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+    parkingLots.add(new ParkingLot(1));
+    parkingLots.add(new ParkingLot(2));
+    Attendant attendant = new Attendant(parkingLots);
+    attendant.park();
+    assertTrue(attendant.park());
+  }
 }
